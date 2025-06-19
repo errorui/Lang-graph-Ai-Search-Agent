2ce241906135a8851285cbfefc1881238e2ce52e
r <rajraman21211@gmail.com>
2025-06-19 12:46:46 +0530

    port binding

--- server/app.py
+++ server/app.py
@@ -235,6 +235,8 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",            # filename:app
               # or "127.0.0.1" for local only
+
+        host="0.0.0.0"      
         port=8000,
         reload=True            # optional: auto-reload on code change (dev only)
     )
