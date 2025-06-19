cf52ba50173499a44749bf08e383cd3e2878a762
r <rajraman21211@gmail.com>
2025-06-19 12:54:55 +0530

    port  binding2

--- server/app.py
+++ server/app.py
@@ -236,7 +236,7 @@ if __name__ == "__main__":
         "app:app",            # filename:app
               # or "127.0.0.1" for local only
 
-        host="0.0.0.0"      
+        host="0.0.0.0",      
         port=8000,
         reload=True            # optional: auto-reload on code change (dev only)
     )
